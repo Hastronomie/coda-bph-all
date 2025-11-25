5cd2a28596a953d551c56bba19b60bb7d82928b0
Mathilde <marco.mathilde@orange.fr>
2025-11-25 10:37:28 +0100

    exo2

---
+++ coda-bph-j4/requetes/exo2/index.php
@@ -0,0 +1,24 @@
+<?php
+$host = "localhost";
+$port = "3306";
+$dbname = "sqlintro";
+$connexionString = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+$user = "root";
+$password = "";
+
+$db = new PDO(
+    $connexionString,
+    $user,
+    $password
+);
+
+$query = $db->prepare('SELECT * FROM users ORDER BY id DESC LIMIT 1;');
+$parameters = [
+    // 'id' => $_GET['id']
+];
+
+$query->execute($parameters);
+$users = $query->fetch(PDO::FETCH_ASSOC);
+var_dump($users)
+?>
